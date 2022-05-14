9ab0535642ba49774f9298330ecb0e6aa7f53d19
kkuyu <devils67@naver.com>
2022-05-14 12:49:55 +0900

    Header part Three

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link, useMatch } from "react-router-dom";
 import styled from "styled-components";
 import { motion } from "framer-motion";
@@ -48,7 +49,7 @@ const Item = styled.li`
   }
 `;
 
-const Circle = styled.span`
+const Circle = styled(motion.span)`
   position: absolute;
   bottom: -8px;
   left: 0;
@@ -60,13 +61,29 @@ const Circle = styled.span`
   background-color: ${(props) => props.theme.red};
 `;
 
-const Search = styled.button`
-  color: white;
-  border: none;
-  background: none;
+const Search = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  button {
+    padding: 0;
+    color: white;
+    border: none;
+    background: none;
+  }
   svg {
+    display: block;
     height: 25px;
   }
+`;
+
+const Input = styled(motion.input)`
+  height: 25px;
+  padding: 0;
+  color: white;
+  background: none;
+  border: none;
+  border-bottom: 1px solid white;
 `;
 
 const logoVariants = {
@@ -85,8 +102,12 @@ const logoVariants = {
 };
 
 function Header() {
+  const [searchOpen, setSearchOpen] = useState(false);
+
   const homeMatch = useMatch("/");
   const tvMatch = useMatch("/tv");
+
+  const toggleSearch = () => setSearchOpen((prev) => !prev);
 
   return (
     <Nav>
@@ -108,24 +129,25 @@ function Header() {
           <Item>
             <Link to="/">
               <span>Home</span>
-              {homeMatch && <Circle />}
+              {homeMatch && <Circle layoutId="circle" />}
             </Link>
           </Item>
           <Item>
             <Link to="/tv">
               <span>Tv Shows</span>
-              {tvMatch && <Circle />}
+              {tvMatch && <Circle layoutId="circle" />}
             </Link>
           </Item>
         </Items>
       </Column>
-      <Column>
-        <Search>
+      <Search>
+        <button onClick={toggleSearch}>
           <svg role="img" aria-label="search" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
             <path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path>
           </svg>
-        </Search>
-      </Column>
+        </button>
+        <Input transition={{ type: "linear" }} animate={{ width: searchOpen ? "190px" : "0px", marginLeft: searchOpen ? "10px" : "0px" }} placeholder="Search for movie or tv show..." />
+      </Search>
     </Nav>
   );
 }
